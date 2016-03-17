15c2a463a17903e0f56bc8f4dcf4d0dc717702cf
rnaxu <luv.s2.rnaxu@gmail.com>
2016-03-17 15:09:42 +0900

    fix

--- README.md
+++ README.md
@@ -2,3 +2,81 @@
 <a href="https://developers.google.com/maps/documentation/javascript/">Google Maps Javascript API</a>
 を使って緯度・経度から住所を取得する  
 例 `東京都千代田区丸の内１丁目９−１`
+
+## 手順
+
+### 座標を渡して`results`を受け取る
+```
+var geocoder = new google.maps.Geocoder;
+
+geocoder.geocode({'location': position}, function(results, status) {
+  if (status === google.maps.GeocoderStatus.OK) {
+    // resultsが返ってきたときの処理
+  } else {
+    // resultsが返ってこなかったときの処理
+    window.alert('Geocoder failed due to: ' + status);
+  }
+});
+```
+
+### `results`が返ってきたとき
+`results`は次のような構造のオブジェクトの集まり  
+オブジェクトの個数は座標によってまちまち
+```
+address_components: Array[n]
+formatted_address: String
+geometry: Object
+place_id: String
+types: Array[n]
+```
+`東京都千代田区丸の内１丁目９−１`のような住所を取得するために必要な情報は`types`と`formatted_address`  
+
+`results`の各オブジェクトの`types`を見て、`types`が`sublocality_level_~`だったらそのオブジェクトの`formatted_address`を使う
+```
+var address = '';
+var resultLength = results.length;
+
+for (var i = 0; i < resultLength; i++) {
+
+    // typesがsublocality_level_~だったら
+    if(results[i].types[0].match(/^sublocality_level_/)) {
+
+        address = formatAddress(results[i].formatted_address);
+
+        // マッチしたらfor文強制終了させる
+        i = resultLength;
+    }
+}
+```
+`types`が`sublocality_level_~`のオブジェクトが存在しなかったら`types`が`premise`の`formatted_address`を使う  
+
+`premise`も存在しない場合、期待する形の住所を生成できないので「No results found」とアラート
+```
+// typesにsublocality_level_~が存在しない場合
+if(address === '') {
+    if(results[0].types[0].match(/premise/)) { // typesがpremiseだったら
+        address = formatAddress(results[0].formatted_address);
+    } else { // それ以外
+        window.alert('No results found');
+    }
+}
+```
+`formatted_address`は`"日本, 〒100-0005 東京都千代田区丸の内１丁目９−１"`のような形になっているので、日本と郵便番号を取り除く  
+外国は今回対象としないので「No results found」とアラート
+```
+function formatAddress(originalAdd) {
+  var formattedAdd = '';
+
+  if(originalAdd.match(/^日本/)) { // 日本国内だったら
+
+    // '日本'と郵便番号を取り除く
+    formattedAdd = originalAdd.replace('日本, ', '').replace(/〒[0-9]{3}-[0-9]{4} /, '');
+
+  } else { // 外国だったら
+    window.alert('No results found');
+  }
+
+  return formattedAdd;
+}
+```
+JSの詳細は<a href="https://github.com/rnaxu/reverse-geocoding/blob/master/src/js/map.js">こちら</a>
--- src/js/map.js
+++ src/js/map.js
@@ -1,124 +1,134 @@
 (function() {
 
-  /* Mapクラス */
-  var Map = {
-
-    // Map生成
-    gmap: new google.maps.Map(document.getElementById('map'), {
+  /**
+   * Mapクラス
+   */
+  var Map = function(options) {
+    this.gmap = new google.maps.Map(document.getElementById('map'), {
       center: {
-        lat: 35.681382,
-        lng: 139.766084
+        lat: options.lat,
+        lng: options.lng
       },
-      zoom: 15
-    }),
+      zoom: options.zoom
+    });
+    this.infowindows = [];
+  };
 
-    // infowindowを格納しておく配列
-    infowindows: [],
+  // 地図のイベントをまとめたやつ
+  Map.prototype.mapEvent = function() {
+    var _this = this;
+    var loadFlag;
 
-    // GoogleMapの中心の緯度と経度を取得
-    getCenteLatLng: function() {
+    // 地図の中心が動いた時
+    this.gmap.addListener('center_changed', function() {
+      clearTimeout(loadFlag);
+      loadFlag = setTimeout(function(){
+        var position = _this.getCenteLatLng();
+        _this.getAddress(position);
+      }, 1000);
+    });
+
+    // 地図がクリックされた時
+    this.gmap.addListener('click', function() {
+      for(var i = 0; i < _this.infowindows.length; i++) {
+        _this.infowindows[i].close();
+      }
+    });
+  };
+
+  // GoogleMapの中心の緯度と経度を取得
+  Map.prototype.getCenteLatLng = function() {
       var centerLtLn = this.gmap.getCenter();
       return {
           lat: centerLtLn.lat(),
           lng: centerLtLn.lng()
       };
-    },
+  };
 
-    // 座標から住所を取得
-    getAddress: function(latLng) {
-      var _this = this;
-      this.noAddress = '地図で表示しているエリア';
-      var geocoder = new google.maps.Geocoder;
-      var position = {
-          lat: latLng.lat,
-          lng: latLng.lng
-      };
-      geocoder.geocode({'location': position}, function(results, status) {
+  // 座標から住所を取得
+  Map.prototype.getAddress = function(latLng) {
+    var _this = this;
+    var geocoder = new google.maps.Geocoder;
+    var position = {
+        lat: latLng.lat,
+        lng: latLng.lng
+    };
+    geocoder.geocode({'location': position}, function(results, status) {
+
+      if (status === google.maps.GeocoderStatus.OK) {
         var address = '';
+        var resultLength = results.length;
 
-        if (status === google.maps.GeocoderStatus.OK) {
+        for (var i = 0; i < resultLength; i++) {
 
-          var resultLength = results.length;
+            // typesがsublocality_level_~だったら
+            if(results[i].types[0].match(/^sublocality_level_/)) {
 
-          for (var i = 0; i < resultLength; i++) {
+                address = _this.formatAddress(results[i].formatted_address);
 
-              // typesがsublocality_level_~だったら
-              if(results[i].types[0].match(/^sublocality_level_/)) {
+                // マッチしたらfor文強制終了させる
+                i = resultLength;
+            }
+        }
 
-                  address = _this.formatAddress(results[i].formatted_address);
-
-                  // マッチしたらfor文強制終了させる
-                  i = resultLength;
-              }
-          }
-
-          // typesにsublocality_level_~が存在しない場合
-          if(address === '') {
-              // typesがpremiseだったら
-              if(results[0].types[0].match(/premise/)) {
-                  address = _this.formatAddress(results[0].formatted_address);
-              } else {
-                  address = _this.noAddress;
-              }
-          }
-
-        } else {
-          address = _this.noAddress;
+        // typesにsublocality_level_~が存在しない場合
+        if(address === '') {
+            if(results[0].types[0].match(/premise/)) {　// typesがpremiseだったら
+                address = _this.formatAddress(results[0].formatted_address);
+            } else { // それ以外
+                window.alert('No results found');
+            }
         }
 
         _this.showInfoWindow(address, position);
-      });
-    },
 
-    // GoogleMapから返ってきた住所を使いやすいように編集
-    formatAddress: function(originalAdd) {
-      var _this = this;
-      var formattedAdd = '';
-
-      if(originalAdd.match(/^日本/)) { // 日本国内だったら
-
-        // '日本'と郵便番号を取り除く
-        formattedAdd = originalAdd.replace('日本, ', '').replace(/〒[0-9]{3}-[0-9]{4} /, '');
-
-      } else { // 外国だったら
-        formattedAdd = _this.noAddress;
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
       }
+    });
 
-      return formattedAdd;
-    },
+  };
 
-    // インフォウィンドウを表示
-    showInfoWindow: function(contentString, latLng) {
-      this.infowindow = new google.maps.InfoWindow({
-        content: contentString,
-        position: latLng
-      });
-      this.infowindows.push(this.infowindow);
-      this.infowindow.open(this.gmap);
+  // GoogleMapから返ってきた住所を使いやすいように編集
+  Map.prototype.formatAddress = function(originalAdd) {
+    var _this = this;
+    var formattedAdd = '';
+
+    if(originalAdd.match(/^日本/)) { // 日本国内だったら
+
+      // '日本'と郵便番号を取り除く
+      formattedAdd = originalAdd.replace('日本, ', '').replace(/〒[0-9]{3}-[0-9]{4} /, '');
+
+    } else { // 外国だったら
+      window.alert('No results found');
     }
+
+    return formattedAdd;
+  };
+
+  // インフォウィンドウを表示
+  Map.prototype.showInfoWindow = function(contentString, latLng) {
+    this.infowindow = new google.maps.InfoWindow({
+      content: contentString,
+      position: latLng
+    });
+    this.infowindows.push(this.infowindow);
+    this.infowindow.open(this.gmap);
   };
 
 
-  /* 実行部分 */
-  var position = Map.getCenteLatLng();
-  Map.getAddress(position);
+  /**
+   * 実行部分
+   */
+  var options = {
+    lat: 35.681382,
+    lng: 139.766084,
+    zoom: 15
+  };
 
-  var loadFlag;
-
-  // 地図の中心が動いた時
-  Map.gmap.addListener('center_changed', function() {
-    clearTimeout(loadFlag);
-    loadFlag = setTimeout(function(){
-      var position = Map.getCenteLatLng();
-      Map.getAddress(position);
-    }, 1000);
-  });
-
-  // 地図がクリックされた時
-  Map.gmap.addListener('click', function() {
-    for(var i = 0; i < Map.infowindows.length; i++) {
-      Map.infowindows[i].close();
-    }
-  });
+  // Mapをインスタンス化
+  var map = new Map(options);
+  map.getAddress(options);
+  map.mapEvent();
 
 })();
